7a7b528ff7546730294ac0396b080e786558696f
Jian Yang Lee <leejyang00@gmail.com>
2024-09-20 18:36:50 +1000

    first commit

---
+++ server/index.ts
@@ -0,0 +1,12 @@
+import app from "./app";
+
+Bun.serve({
+  // port: 8080, // defaults to $BUN_PORT, $PORT, $NODE_PORT otherwise 3000
+  // hostname: "mydomain.com", // defaults to "0.0.0.0"
+  // fetch(req) {
+  //   return new Response("Hello from bun server");
+  // },
+  fetch: app.fetch,
+});
+
+console.log("Server is running on port 3000");
---
+++ server/routes/expenses.ts
@@ -0,0 +1,50 @@
+import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+
+const expenseSchema = z.object({
+  id: z.number().int().positive().min(1),
+  title: z.string().min(3).max(100),
+  amount: z.number().int().positive(),
+});
+
+type Expense = z.infer<typeof expenseSchema>;
+const createPostSchema = expenseSchema.omit({ id: true });
+
+const fakeExpenses: Expense[] = [
+  { id: 1, title: "Groceries", amount: 100 },
+  { id: 2, title: "Rent", amount: 1000 },
+  { id: 3, title: "Utilities", amount: 200 },
+];
+
+export const expensesRoute = new Hono()
+  .get("/", (c) => {
+    return c.json({ expense: [fakeExpenses] });
+  })
+  .get("/:id{[0-9]+}", (c) => {
+    const id = Number.parseInt(c.req.param("id"));
+    const expense = fakeExpenses.find((expense) => expense.id === id);
+    if (!expense) {
+      return c.notFound();
+    }
+    return c.json({ expense });
+  })
+  .get("/total-spent", (c) => {
+    const total = fakeExpenses.reduce((acc, expense) => acc + expense.amount, 0);
+    return c.json({ total });
+  })
+  .post("/", zValidator("json", createPostSchema), async (c) => {
+    const expense = await c.req.valid("json");
+    fakeExpenses.push({ ...expense, id: fakeExpenses.length + 1 });
+    c.status(201);
+    return c.json(expense);
+  })
+  .delete("/:id{[0-9]+}", (c) => {
+    const id = Number.parseInt(c.req.param("id"));
+    const index = fakeExpenses.findIndex((expense) => expense.id === id);
+    if (index === -1) {
+      return c.notFound();
+    }
+    const deletedExpense = fakeExpenses.splice(index, 1)[0];
+    return c.json({ expense: deletedExpense });
+  });
