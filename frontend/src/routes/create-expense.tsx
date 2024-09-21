db254a92d8204a42e5f1fff9f21a4ee89cd6d7ab
Jian Yang Lee <leejyang00@gmail.com>
2024-09-22 02:11:30 +1000

    tanstack forms

--- frontend/src/routes/create-expense.tsx
+++ frontend/src/routes/create-expense.tsx
@@ -1,22 +1,112 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Button } from "@/components/ui/button"
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
+import { useForm } from "@tanstack/react-form";
+import { api } from "@/lib/api";
 
 export const Route = createFileRoute("/create-expense")({
   component: CreateExpense,
 });
 
 function CreateExpense() {
+  const navigate = useNavigate();
+
+  const form = useForm({
+    defaultValues: {
+      title: "",
+      amount: 0,
+    },
+    onSubmit: async ({ value }) => {
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+
+      const res = await api.expenses.$post({ json: value });
+      if (!res.ok) {
+        throw new Error("Failed to create expense");
+      }
+      navigate({ to: "/expenses" });
+    },
+  });
   return (
     <div className="p-2">
       <h2>Create Expense</h2>
-      <form className="m-auto max-w-xl">
-        <Label htmlFor="title">Title</Label>
-        <Input type="text" id="title" placeholder="Title" />
-        <Label htmlFor="amount">Amount</Label>
-        <Input type="number" id="amount" placeholder="Amount" /> 
-        <Button type="submit">Create Expense</Button>
+      <form
+        className="m-auto max-w-xl"
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+      >
+        {/* A type-safe field component*/}
+        <form.Field
+          name="title"
+          // validators={{
+          //   onChange: ({ value }) =>
+          //     !value
+          //       ? "A first name is required"
+          //       : value.length < 3
+          //         ? "First name must be at least 3 characters"
+          //         : undefined,
+          //   onChangeAsyncDebounceMs: 500,
+          //   onChangeAsync: async ({ value }) => {
+          //     await new Promise((resolve) => setTimeout(resolve, 1000));
+          //     return (
+          //       value.includes("error") && 'No "error" allowed in first name'
+          //     );
+          //   },
+          // }}
+          children={(field) => {
+            // Avoid hasty abstractions. Render props are great!
+            return (
+              <>
+                <Label htmlFor={field.name}>Title</Label>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {field.state.meta.isTouched &&
+                field.state.meta.errors.length ? (
+                  <em>{field.state.meta.errors.join(", ")}</em>
+                ) : null}
+              </>
+            );
+          }}
+        />
+        <form.Field
+          name="amount"
+          children={(field) => {
+            return (
+              <>
+                <Label htmlFor={field.name}>Amount</Label>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  type="number"
+                  onChange={(e) => field.handleChange(Number(e.target.value))}
+                />
+                {field.state.meta.isTouched &&
+                field.state.meta.errors.length ? (
+                  <em>{field.state.meta.errors.join(", ")}</em>
+                ) : null}
+              </>
+            );
+          }}
+        />
+        <form.Subscribe
+          selector={(state) => [state.canSubmit, state.isSubmitting]}
+          children={([canSubmit, isSubmitting]) => (
+            <Button type="submit" disabled={!canSubmit}>
+              {isSubmitting ? "..." : "Submit"}
+            </Button>
+          )}
+        />
       </form>
     </div>
   );
